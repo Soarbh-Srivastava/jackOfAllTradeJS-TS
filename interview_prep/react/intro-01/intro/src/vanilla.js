a67a379c8a43e0c469717ba859b7ebe43221069e
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-06 14:15:22 +0530

    learn about jsx and createElement

--- interview_prep/react/intro-01/intro/src/vanilla.js
+++
@@ -1,5 +0,0 @@
-//old js way imperative way we tell how to make h1 and add to ui
-const root = document.getElementById("root");
-const h1 = document.createElement("h1");
-h1.innerHTML("hello world");
-root.appendChild(h1);
