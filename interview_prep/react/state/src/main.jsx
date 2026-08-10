3e9f3c2062462bd0e9e839e5608cbf34add7d100
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-10 11:41:24 +0530

    Learned: drived state

--- interview_prep/react/state/src/main.jsx
+++ interview_prep/react/state/src/main.jsx
@@ -38,3 +38,31 @@ import { useContext } from "react";
 // =================================
 // ========== Drived State =========
 // =================================
+
+const App = () => {
+  const [users, setUsers] = useState(["Alice", "Bob", "Charlie"]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // BAD: Creating redundant state for the derived value
+  // const [filteredUsers, setFilteredUsers] = useState([]);
+
+  // GOOD: Calculating Derived State on the fly
+  // This recalculates automatically whenever users or searchQuery changes
+  const filteredUsers = users.filter((user) =>
+    user.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
