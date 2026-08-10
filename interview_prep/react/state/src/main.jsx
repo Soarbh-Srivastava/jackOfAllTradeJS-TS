fa017ae9c665fd6bc9d0df44444e68b01bec969b
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-10 11:20:51 +0530

    learned: gloabal context using context api

--- interview_prep/react/state/src/main.jsx
+++ interview_prep/react/state/src/main.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
+import { useContext } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
 
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   );
+// };
+
+const ThemeContext = React.createContext();
+const App = () => {
+  const [theme, setTheme] = useState("light");
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <MainContain></MainContain>
+    </ThemeContext.Provider>
+  );
+};
+
+const MainContain = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <>
+      <p>{theme}</p>
+    </>
   );
 };
