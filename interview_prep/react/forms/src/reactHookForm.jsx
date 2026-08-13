6543b0f2e62624ad56a011f2df4a2117b8a5b564
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-13 14:32:14 +0530

    Learned : differnent input type

--- interview_prep/react/forms/src/reactHookForm.jsx
+++ interview_prep/react/forms/src/reactHookForm.jsx
@@ -1,31 +1,66 @@
 import { useForm } from "react-hook-form";
 
+// export const ReactHookForm = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm({
+//     defaultValues: {
+//       firstName: "",
+//       lastName: "",
+//     },
+//   });
+
+//   return (
+//     <form onSubmit={handleSubmit((data) => console.log(data))}>
+//       <input
+//         {...register("lastname", {
+//           required: "this required",
+//           minLength: {
+//             value: 10,
+//             message: "minLength should less than 10",
+//           },
+//         })}
+//       />
+//       <p>{errors.firstName?.message}</p>
+//       <input {...register("firstName")} />
+//       <button>Submit</button>
+//     </form>
+//   );
+// };
+
 export const ReactHookForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit } = useForm({
     defaultValues: {
-      firstName: "",
-      lastName: "",
+      fName: "",
+      lName: "",
+      category: "",
+      checbox: [],
+      radio: "",
     },
   });
-
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(console.log)}>
       <input
-        {...register("lastname", {
-          required: "this required",
-          minLength: {
-            value: 10,
-            message: "minLength should less than 10",
-          },
-        })}
+        {...register("fName", { required: true })}
+        placeholder="First Name"
       />
-      <p>{errors.firstName?.message}</p>
-      <input {...register("firstName")} />
-      <button>Submit</button>
+      <input {...register("lName", { minLength: 5 })} placeholder="Last Name" />
+      <select {...register("category")}>
+        <option value=""></option>
+        <option value="a"> option A</option>
+        <option value="b">option B</option>
+      </select>
+      <input {...register("checbox")} type="checkbox" />
+      <input {...register("checbox")} type="checkbox" />
+      <input {...register("checkbox")} type="checkbox" />
+
+      <input {...register("radio")} type="radio" value="A" />
+      <input {...register("radio")} type="radio" value="B"></input>
+      <input {...register("radio")} type="radio" value="C" />
+
+      <input type="submit" />
     </form>
   );
 };
