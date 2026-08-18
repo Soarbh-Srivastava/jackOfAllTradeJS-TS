b4c0df5236ba8850f58b566922d6b0e871fe50c5
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-18 12:37:27 +0530

    learned: how use type with component and figure out using intellije

--- interview_prep/Ts+React/react-typescript-labs/src/examples/accident-counter/counter.tsx
+++ interview_prep/Ts+React/react-typescript-labs/src/examples/accident-counter/counter.tsx
@@ -1,21 +1,42 @@
 import { Card } from '$/common/components/card';
+import React, { useState, type FormEvent } from 'react';
 import { Button } from './button';
 
+type CounterControllerProps = {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const CounterController = ({ setCount }: CounterControllerProps): React.ReactNode => {
+  return (
+    <div className="flex gap-2">
+      <button onClick={() => setCount((prev) => prev - 1)}>➖ Decrement</button>
+      <button onClick={() => setCount(0)}>🔁 Reset</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>➕ Increment</button>
+    </div>
+  );
+};
+
 export const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+  const [draftCount, setDraftCount] = useState<number>(0);
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
-      <div className="flex gap-2">
-        <Button>➖ Decrement</Button>
-        <Button>🔁 Reset</Button>
-        <Button>➕ Increment</Button>
-      </div>
-      <form className="flex items-center gap-2" onSubmit={(e) => e.preventDefault()}>
+      <p className="text-6xl">{count}</p>
+      <CounterController setCount={setCount} />
+
+      <form
+        className="flex items-center gap-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCount(draftCount);
+        }}
+      >
         <input
           className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
           type="number"
-          value={0}
+          onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+          value={draftCount}
         />
         <Button>Update Counter</Button>
       </form>
