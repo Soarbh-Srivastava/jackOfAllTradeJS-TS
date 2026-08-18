ab5f083d9eb9cd706d6ed5e1825010588b8e93e3
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-18 13:53:36 +0530

    learned and Build : custome button with the ts

--- interview_prep/Ts+React/react-typescript-labs/src/examples/accident-counter/counter.tsx
+++ interview_prep/Ts+React/react-typescript-labs/src/examples/accident-counter/counter.tsx
@@ -6,13 +6,38 @@ type CounterControllerProps = {
   setCount: React.Dispatch<React.SetStateAction<number>>;
 };
 
+type FormControllerProps = CounterControllerProps & {
+  draftCount: number;
+  setDraftCount: React.Dispatch<React.SetStateAction<number>>;
+};
+
 const CounterController = ({ setCount }: CounterControllerProps): React.ReactNode => {
   return (
     <div className="flex gap-2">
-      <button onClick={() => setCount((prev) => prev - 1)}>➖ Decrement</button>
-      <button onClick={() => setCount(0)}>🔁 Reset</button>
-      <button onClick={() => setCount((prev) => prev + 1)}>➕ Increment</button>
+      <Button onClick={() => setCount((prev) => prev - 1)}>➖ Decrement</Button>
+      <Button onClick={() => setCount(0)}>🔁 Reset</Button>
+      <Button onClick={() => setCount((prev) => prev + 1)}>➕ Increment</Button>
     </div>
+  );
+};
+
+const FormController = ({ draftCount, setCount, setDraftCount }: FormControllerProps) => {
+  return (
+    <form
+      className="flex items-center gap-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        setCount(draftCount);
+      }}
+    >
+      <input
+        className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
+        type="number"
+        onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+        value={draftCount}
+      />
+      <Button>Update Counter</Button>
+    </form>
   );
 };
 
@@ -24,22 +49,7 @@ export const Counter = () => {
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{count}</p>
       <CounterController setCount={setCount} />
-
-      <form
-        className="flex items-center gap-2"
-        onSubmit={(e) => {
-          e.preventDefault();
-          setCount(draftCount);
-        }}
-      >
-        <input
-          className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
-          type="number"
-          onChange={(e) => setDraftCount(e.target.valueAsNumber)}
-          value={draftCount}
-        />
-        <Button>Update Counter</Button>
-      </form>
+      <FormController draftCount={draftCount} setCount={setCount} setDraftCount={setDraftCount} />
     </Card>
   );
 };
