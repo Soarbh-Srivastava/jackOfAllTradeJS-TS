0726e225d6e2dfb6c23ce938ab3b11836ed29c3a
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-20 16:27:16 +0530

    next init

---
+++ interview_prep/intermidateReact/rcs-tut/src/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link herf="/my">My Notes</Link>
+          <Link herf="/write-a-notes">Write a Notes</Link>
+          <Link herf="/secret-teacher-feed">Secret Teacher Feed </Link>
+          <Link herf="/who-i-am">Who i am ?</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
