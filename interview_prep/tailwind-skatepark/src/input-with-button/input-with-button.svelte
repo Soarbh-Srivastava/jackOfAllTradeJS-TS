996f07298807de6a96b2d4a042b02168c2209674
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-25 15:13:41 +0530

    tailwind: input box with button

--- interview_prep/tailwind-skatepark/src/input-with-button/input-with-button.svelte
+++ interview_prep/tailwind-skatepark/src/input-with-button/input-with-button.svelte
@@ -1,5 +1,5 @@
-<form>
-  <label for="new-task">What do you need to do?</label>
-  <input type="text" id="new-task" placeholder="New task…" />
-  <button>Submit</button>
+<form class="flex border p-2">
+  <label for="new-task" class="sr-only">What do you need to do?</label>
+  <input type="text" class="flex-1" id="new-task" placeholder="New task…" />
+  <button class="bg-purple-500 px-2 py-1 text-amber-50">Submit</button>
 </form>
