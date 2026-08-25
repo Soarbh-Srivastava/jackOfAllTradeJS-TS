8422c8f7b922a5d5dbe74260f86ba554c4c95de0
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-25 15:24:44 +0530

    learned: flex

--- interview_prep/tailwind-skatepark/src/card-list/card-list.svelte
+++ interview_prep/tailwind-skatepark/src/card-list/card-list.svelte
@@ -2,7 +2,7 @@
   import Card from '../card/card.svelte';
 </script>
 
-<div class="space-y-4">
+<div class="flex flex-col gap-2 sm:flex-row">
   <Card title="Card Title" description="This is a description of the card." />
   <Card title="Another Card" description="This is another description of a different card." />
   <Card title="Third Card" description="This is a description of the third card." />
