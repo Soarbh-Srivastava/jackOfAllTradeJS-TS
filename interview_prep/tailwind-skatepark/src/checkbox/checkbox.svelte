df1d838cc9f0b0ae5ba9647c18ff337fa2ba64a3
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-25 14:59:07 +0530

    check box

--- interview_prep/tailwind-skatepark/src/checkbox/checkbox.svelte
+++ interview_prep/tailwind-skatepark/src/checkbox/checkbox.svelte
@@ -2,7 +2,14 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div>
+<div class="group border bg-slate-100 p-4">
   <input {id} {...props} type="checkbox" />
-  <label for={id}>{label}</label>
+  <label for={id} class="group-has-[:checked]:line-through">{label}</label>
+
+  <!-- Fixed the colon here: group-has-[:checked] -->
+  <div class="hidden flex-row space-x-2 group-has-[:checked]:flex">
+    <label class="block">what is your reason ?</label>
+    <input class="rounded-md border" />
+    <button>Submit </button>
+  </div>
 </div>
