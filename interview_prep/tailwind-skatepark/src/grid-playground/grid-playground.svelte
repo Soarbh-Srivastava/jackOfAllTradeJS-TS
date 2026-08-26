d83f6795cf785972a7da7d0a142bfd69dc9b5eb1
Soarbh-Srivastava <srivastav.soarbh@gmail.com>
2026-08-26 11:07:20 +0530

    learned: basic use of grid

--- interview_prep/tailwind-skatepark/src/grid-playground/grid-playground.svelte
+++ interview_prep/tailwind-skatepark/src/grid-playground/grid-playground.svelte
@@ -1,11 +1,7 @@
-<div class="space-y-4">
-  <div class="flex items-center justify-center rounded-md bg-sky-300 p-4">1</div>
+<div class=" grid gap-4 md:h-screen md:grid-cols-[8rem_1fr_8rem]">
+  <div class="flex items-center justify-center rounded-md bg-sky-300 p-4 md:col-span-3">1</div>
   <div class="flex items-center justify-center rounded-md bg-pink-300 p-4">2</div>
   <div class="flex items-center justify-center rounded-md bg-cyan-300 p-4">3</div>
   <div class="flex items-center justify-center rounded-md bg-indigo-300 p-4">4</div>
-  <div class="flex items-center justify-center rounded-md bg-emerald-300 p-4">5</div>
-  <div class="flex items-center justify-center rounded-md bg-amber-300 p-4">6</div>
-  <div class="flex items-center justify-center rounded-md bg-rose-300 p-4">7</div>
-  <div class="flex items-center justify-center rounded-md bg-violet-300 p-4">8</div>
-  <div class="flex items-center justify-center rounded-md bg-lime-300 p-4">9</div>
+  <div class="flex items-center justify-center rounded-md bg-indigo-300 p-4 md:col-span-3">4</div>
 </div>
